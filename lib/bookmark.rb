64a26280d1744cdda29c99a72d44aeed614ba3bf
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-24 18:18:01 +0100

    added link to database in bookmark class, fully replaces hardcoded bookmark list in self.all method, passes tests

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,10 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "https://www.google.co.uk",
-      "https://www.reddit.com"
-      ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 end

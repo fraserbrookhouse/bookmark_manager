d0fa0d9c45796dbbb0d1744080d2f37d1311f7f7
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-25 17:16:22 +0100

    added button to bookmarks page to add new bookmark so you don't need to navigate via the url, updated tests, passes tests

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -1,6 +1,7 @@
 feature 'Adding bookmarks' do
   scenario 'A user can add bookmarks to the bookmark manager' do
-    visit('/bookmarks/new')
+    visit('/bookmarks')
+    click_button('Add new')
     fill_in('url', with: 'http://testbookmark.com')
     click_button('Submit')
 

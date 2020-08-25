b1c9be7f3e00ffadc3dbac56d68eaa8cd9272c82
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-25 16:16:02 +0100

    added feature test for add bookmark feature, fails test

---
+++ spec/features/creating_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+feature 'Adding bookmarks' do
+  scenario 'A user can add bookmarks to the bookmark manager' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'http://testbookmark.com')
+    click_button('Submit')
+
+    expect(page).to have_content 'http://testbookmark.com'
+  end
+end

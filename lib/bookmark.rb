569aacb4a5c87c409269565620d88963024bd811
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-26 14:12:54 +0100

    fixed type in self.create, changed title to url, now passes all tests

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -29,7 +29,7 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end

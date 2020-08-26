b9fbfedadd0e913e7b1168384faed67a95e03bd9
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-26 17:50:50 +0100

    added delete logic to controller, passes tests

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'shotgun'
 require_relative './lib/bookmark'
 
 class Bookmark_Manager < Sinatra::Base
-  enable :sessions
+  enable :sessions, :method_override
 
   get '/' do
     erb :index
@@ -22,6 +22,12 @@ class Bookmark_Manager < Sinatra::Base
 
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("DELETE FROM bookmarks WHERE id = #{params['id']}")
     redirect '/bookmarks'
   end
 end
